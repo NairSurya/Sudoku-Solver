b12ab002789009db406518cbe5e30b748331874f
Surya Nair <surya.nair@mobiuso.com>
2016-03-29 19:48:47 +0530

    Adding Custom Text Field

---
+++ Sudoku-Solver/CustomTextField.swift
@@ -0,0 +1,30 @@
+//
+//  CustomTextField.swift
+//  Sudoku-Solver
+//
+//  Created by Surya on 29/03/16.
+//  Copyright © 2016 Surya LLC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CustomTextField: UITextField{
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        //Border
+        self.layer.cornerRadius = 15.0;
+        self.layer.borderWidth = 1.5
+        self.layer.borderColor = UIColor.whiteColor().CGColor
+        
+        //Background
+        self.backgroundColor = UIColor(white: 1, alpha: 0.0)
+        
+        //Text
+        self.textColor = UIColor.whiteColor()
+        self.textAlignment = NSTextAlignment.Center
+    }
+    
+}
