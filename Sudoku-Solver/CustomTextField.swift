18b7d0243a5e6c2e884af4b803f41ff7b6e5731e
Surya Nair <surya.nair@mobiuso.com>
2016-04-21 20:00:11 +0530

    Dynamic creation of a textview.

--- Sudoku-Solver/CustomTextField.swift
+++ Sudoku-Solver/CustomTextField.swift
@@ -11,13 +11,13 @@ import UIKit
 
 class CustomTextField: UITextField{
     
-    required init?(coder aDecoder: NSCoder) {
+    required init!(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
         //Border
         self.layer.cornerRadius = 15.0;
         self.layer.borderWidth = 1.5
-        self.layer.borderColor = UIColor.whiteColor().CGColor
+        self.layer.borderColor = UIColor.redColor().CGColor
         
         //Background
         self.backgroundColor = UIColor(white: 1, alpha: 0.0)
--- Sudoku-Solver/ViewController.swift
+++ Sudoku-Solver/ViewController.swift
@@ -12,14 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        textField()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBOutlet var mainView: UIView!
+    
+    func textField() {
+        let textField = UITextField(frame: CGRectMake(20.0, 30.0, 100.0, 30.0))
+        textField.textAlignment = NSTextAlignment.Center
+        textField.textColor = UIColor.blueColor()
+        textField.borderStyle = UITextBorderStyle.Line
+        textField.autocapitalizationType = UITextAutocapitalizationType.Words // If you need any capitalization
+        self.mainView.addSubview(textField)
+    }
 }
 
